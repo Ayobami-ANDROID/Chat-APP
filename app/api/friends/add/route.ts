e6e259a093433a3700e8668842f743e0ab1aeb2e
Ayobami-ANDROID <ajetunmobiayobami19@gmail.com>
2024-11-07 14:39:26 +0100

    chanes

--- app/api/friends/add/route.ts
+++ app/api/friends/add/route.ts
@@ -1,4 +1,7 @@
+import { fetchRedis } from "@/app/helpers/redis"
+import { authOptions } from "@/app/lib/auth"
 import { addFriendValidator } from "@/app/lib/valiations/add-friend"
+import { getServerSession } from "next-auth"
 
 export async function Post(req:Request){
     try {
@@ -15,7 +18,29 @@ export async function Post(req:Request){
             }
         )
  
-        const data = await RestResponse.json()
+        const data = await RestResponse.json() as {result:string | null}
+        
+        const session = await getServerSession(authOptions)
+
+        const idToAdd = data.result
+
+        if(!session){
+            return new Response ('Unathorizes',{status:401})
+        }
+
+        if(!idToAdd){
+            return new Response('This person does not exist.',{status:400})
+        }
+
+        if(idToAdd === session.user.id){
+            return new Response('YOu cannot add yourself as a friend')
+        }
+
+
+        const isAlreadyAdded = fetchRedis('sismember',`user:${idToAdd}:incoming_friend_requests`,session.user.id)
+
+
+       
     } catch (error) {
         
     }
---
+++ app/helpers/redis.ts
@@ -0,0 +1,28 @@
+const upstashRedisRestURL = process.env.UPSTASH_REDIS_REST_URL
+const authToken = process.env.UPSTASH_REDIS_REST_TOKEN
+
+type command = 'zrange' | 'sismember' | 'get' | 'smemebers'
+
+
+export async function fetchRedis(
+    command:command,
+    ...args:(string | number | null)[] 
+){
+    const commandUrl = `${upstashRedisRestURL}/${command}/${args.join('/')}`
+
+    const response = await fetch(commandUrl,
+            {
+                headers:{
+                    Authorization:`Bearer ${authToken}`
+                },
+                cache:'no-store'
+            }
+        )
+
+        if (!response.ok){
+            throw new Error (`Error executing Redis command: ${response.statusText}`)
+        }
+
+        const data = await response.json()
+        return data.result
+}
