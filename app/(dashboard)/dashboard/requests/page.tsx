38e0819e384426327bba440457468015202c2aa8
Ayobami-ANDROID <ajetunmobiayobami19@gmail.com>
2024-11-09 22:04:33 +0100

    friend request done

--- app/(dashboard)/dashboard/requests/page.tsx
+++ app/(dashboard)/dashboard/requests/page.tsx
@@ -3,6 +3,7 @@ import { authOptions } from '@/app/lib/auth'
 import { getServerSession } from 'next-auth'
 import { notFound } from 'next/navigation'
 import { FC } from 'react'
+import FriendRequests from '@/app/components/FriendRequests'
 
 
 
@@ -10,11 +11,24 @@ const page: FC = async ({}) => {
     const session = await getServerSession(authOptions)
 
     if(!session) notFound()
-    const IncomingSenderIds = (await fetchRedis('smembers',`user:${session.user.id}:incoming_friend_requests`) as String[]).length
+    const incomingSenderIds = (await fetchRedis('smembers',`user:${session.user.id}:incoming_friend_requests`)) as string[]
 
-    // const incomingFriendRequests =await Promise.all()
+    const incomingFriendRequests =await Promise.all(
+        incomingSenderIds.map(async(senderId) => {
+            const sender = (await fetchRedis('get',`user:${senderId}`)) as User
+            return{
+                 senderId,
+                 senderEmail:sender.email
+            }
+        })
+    )
 
-  return <div>page</div>
+  return  <main className='pt-8'>
+  <h1 className='font-bold text-5xl mb-8'>Add a friend</h1>
+  <div className='flex flex-col gap-4'>
+    <FriendRequests incomingFriendRequests={incomingFriendRequests} sessionId={session.user.id}/>
+  </div>
+</main>
 }
 
 export default page
