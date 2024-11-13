47e5b0877990c76f95ef70217ef6d18c84ec6c9a
Ayobami-ANDROID <ajetunmobiayobami19@gmail.com>
2024-11-13 09:07:34 +0100

    unseenFriend Request

--- app/components/SideBarChatList.tsx
+++ app/components/SideBarChatList.tsx
@@ -1,13 +1,28 @@
-import { FC } from 'react'
+import { usePathname, useRouter } from 'next/navigation'
+import { FC,useState,useEffect } from 'react'
 
 interface SideBarChatListProps {
   friends:User[]
 }
 
 const SideBarChatList: FC<SideBarChatListProps> = ({friends}) => {
+    const router = useRouter()
+    const pathname = usePathname()
+    const [unseenMessages,setUnseenMessages] = useState<Message[]>([])
+
+    useEffect(() => {
+        if(pathname?.includes('chat')){
+            setUnseenMessages((prev) =>{
+                return prev.filter((msg) => !pathname.includes(msg.senderId))
+            })
+        }
+    },[pathname])
   return <ul role="list" className="max-h-[25rem] overflow-y-auto -mx-2 space-y-1">
      {friends.sort().map((friend) =>{
-         return <div></div>
+        const unseenMessagesCount = unseenMessages.filter((unseenMsg) => {
+            return unseenMsg.senderId === friend.id
+        }).length
+         return <li key={friend.id}></li>
      })}
   </ul>
 }
