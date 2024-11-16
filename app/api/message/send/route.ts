81ed950c198726ff43282ee690ddaf2fdba1cb7b
Ayobami-ANDROID <ajetunmobiayobami19@gmail.com>
2024-11-16 18:55:50 +0100

    changes

--- app/api/message/send/route.ts
+++ app/api/message/send/route.ts
@@ -1,3 +1,4 @@
+import { fetchRedis } from "@/app/helpers/redis"
 import { authOptions } from "@/app/lib/auth"
 import { getServerSession } from "next-auth"
 
@@ -13,6 +14,18 @@ import { getServerSession } from "next-auth"
         if(session.user.id !== userId1 && session.user.id !==userId2){
             return new Response('Unauthorized',{status:401})
         } 
+
+        const friendId = session.user.id === userId1 ? userId2 : userId1
+
+        const friendList = (await fetchRedis('smembers',`user:${session.user.id}:friends`))as string[]
+        const isFriend = friendList.includes(friendId)
+
+        if(!isFriend){
+            return new Response('Unathorized',{status:401})
+        }
+
+        const sender = await fetchRedis('get',`user:${session.user.id}`) as User
+        console.log("sender",sender)
 
          
     } catch (error) {
